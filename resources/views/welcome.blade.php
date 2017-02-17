abe41a10a8237bc346d23d0d6ac25c7cf3a14994
Alex <alex_sh@kodeks.ru>
2017-02-17 15:59:43 +0300

    giltlab docker build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel!
+                    Laravel! Helloy!
                 </div>
 
                 <div class="links">
